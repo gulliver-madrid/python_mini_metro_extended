dd407e8b1063db2fc418bed6907a37fa409c4527
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-07-08 19:13:59 +0200

    fix TestGameplay name

--- test/test_gameplay.py
+++ test/test_gameplay.py
@@ -15,7 +15,7 @@ from src.mediator import Mediator
 from src.utils import get_random_color, get_random_position
 
 
-class TestMediator(unittest.TestCase):
+class TestGameplay(unittest.TestCase):
     def setUp(self) -> None:
         self.width, self.height = screen_width, screen_height
         self.screen = create_autospec(pygame.surface.Surface)
