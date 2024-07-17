2218321aafa7dab7b8a20abf1bd359ec646b7845
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-07-17 15:16:38 +0200

    use number type alias in point.py

--- src/geometry/point.py
+++ src/geometry/point.py
@@ -6,9 +6,12 @@ from typing import Any
 
 from shortuuid import uuid
 
+Number = int | float
+NumberType = (int, float)
+
 
 class Point:
-    def __init__(self, left: int | float, top: int | float) -> None:
+    def __init__(self, left: Number, top: Number) -> None:
         self.left = left
         self.top = top
         self.id = f"Point-{uuid()}"
@@ -16,35 +19,35 @@ class Point:
     def __repr__(self) -> str:
         return f"Point(left = {self.left}, top = {self.top})"
 
-    def __add__(self, other: Point | int | float) -> Point:
+    def __add__(self, other: Point | Number) -> Point:
         if isinstance(other, Point):
             return Point(self.left + other.left, self.top + other.top)
         else:
-            assert isinstance(other, (int, float))
+            assert isinstance(other, NumberType)
             return Point(self.left + other, self.top + other)
 
-    def __radd__(self, other: Point | int | float) -> Point:
+    def __radd__(self, other: Point | Number) -> Point:
         return self.__add__(other)
 
-    def __sub__(self, other: Point | int | float) -> Point:
+    def __sub__(self, other: Point | Number) -> Point:
         if isinstance(other, Point):
             return Point(self.left - other.left, self.top - other.top)
         else:
-            assert isinstance(other, (int, float))
+            assert isinstance(other, NumberType)
             return Point(self.left - other, self.top - other)
 
-    def __rsub__(self, other: Point | int | float) -> Point:
+    def __rsub__(self, other: Point | Number) -> Point:
         if isinstance(other, Point):
             return Point(other.left - self.left, other.top - self.top)
         else:
-            assert isinstance(other, (int, float))
+            assert isinstance(other, NumberType)
             return Point(other - self.left, other - self.top)
 
-    def __mul__(self, other: int | float) -> Point:
-        assert isinstance(other, (int, float))
+    def __mul__(self, other: Number) -> Point:
+        assert isinstance(other, NumberType)
         return Point(other * self.left, other * self.top)
 
-    def __rmul__(self, other: int | float) -> Point:
+    def __rmul__(self, other: Number) -> Point:
         return self.__mul__(other)
 
     def __eq__(self, other: object) -> bool:
@@ -75,5 +78,5 @@ class Point:
             setattr(result, k, deepcopy(v, memo))
         return result
 
-    def to_tuple(self) -> tuple[int | float, int | float]:
+    def to_tuple(self) -> tuple[Number, Number]:
         return (self.left, self.top)
