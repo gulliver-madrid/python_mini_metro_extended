09750127f86bee8e55d46a6d878aaca18b4da376
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-07-18 15:26:07 +0200

    use class GameRenderer

--- src/mediator/mediator.py
+++ src/mediator/mediator.py
@@ -42,6 +42,7 @@ class Mediator:
         "_passenger_mover",
         "showing_debug",
         "game_speed",
+        "_game_renderer",
     )
 
     _gui_height: Final = get_gui_height()
@@ -68,6 +69,7 @@ class Mediator:
         self.game_speed = 1
         # UI
         self.ui = UI()
+        self._game_renderer = GameRenderer()
 
         # delegated classes
         self.path_manager = PathManager(
@@ -118,7 +120,7 @@ class Mediator:
         self._move_passengers()
 
     def render(self, screen: pygame.surface.Surface) -> None:
-        render_game(
+        self._game_renderer.render_game(
             screen,
             gui_height=self._gui_height,
             main_surface_height=self._main_surface_height,
@@ -180,59 +182,63 @@ class Mediator:
             self._passenger_mover.move_passengers(metro)
 
 
-def render_game(
-    screen: pygame.surface.Surface,
-    *,
-    gui_height: float,
-    main_surface_height: float,
-    paths: Sequence[Path],
-    max_num_paths: int,
-    stations: Sequence[Station],
-    metros: Sequence[Metro],
-    score: int,
-    ui: UI,
-    showing_debug: bool,
-    game_speed: float,
-) -> None:
-    main_surface = screen.subsurface(0, gui_height, screen_width, main_surface_height)
-    main_surface.fill((180, 180, 120))
-    for idx, path in enumerate(paths):
-        path_order = idx - round(max_num_paths / 2)
-        path.draw(screen, path_order)
-    for station in stations:
-        station.draw(screen)
-    for metro in metros:
-        metro.draw(screen)
-    ui.render(screen, score)
-    fps = ui.clock.get_fps() if ui.clock else None
-    if showing_debug:
-        draw_debug(screen, ui.small_font, ui.last_pos, fps, speed=game_speed)
+class GameRenderer:
+    def render_game(
+        self,
+        screen: pygame.surface.Surface,
+        *,
+        gui_height: float,
+        main_surface_height: float,
+        paths: Sequence[Path],
+        max_num_paths: int,
+        stations: Sequence[Station],
+        metros: Sequence[Metro],
+        score: int,
+        ui: UI,
+        showing_debug: bool,
+        game_speed: float,
+    ) -> None:
+        main_surface = screen.subsurface(
+            0, gui_height, screen_width, main_surface_height
+        )
+        main_surface.fill((180, 180, 120))
+        for idx, path in enumerate(paths):
+            path_order = idx - round(max_num_paths / 2)
+            path.draw(screen, path_order)
+        for station in stations:
+            station.draw(screen)
+        for metro in metros:
+            metro.draw(screen)
+        ui.render(screen, score)
+        fps = ui.clock.get_fps() if ui.clock else None
+        if showing_debug:
+            self._draw_debug(screen, ui.small_font, ui.last_pos, fps, speed=game_speed)
 
+    def _draw_debug(
+        self,
+        screen: pygame.surface.Surface,
+        font: pygame.font.Font,
+        mouse_pos: Point | None,
+        fps: float | None,
+        *,
+        speed: float = 1.0,
+    ) -> None:
+        fg_color = (255, 255, 255)
+        bg_color = (0, 0, 0)
+        size = (300, 200)
+        debug_surf = pygame.Surface(size)
+        debug_surf.set_alpha(180)
+        debug_surf.fill(bg_color)
 
-def draw_debug(
-    screen: pygame.surface.Surface,
-    font: pygame.font.Font,
-    mouse_pos: Point | None,
-    fps: float | None,
-    *,
-    speed: float = 1.0,
-) -> None:
-    fg_color = (255, 255, 255)
-    bg_color = (0, 0, 0)
-    size = (300, 200)
-    debug_surf = pygame.Surface(size)
-    debug_surf.set_alpha(180)
-    debug_surf.fill(bg_color)
+        debug_texts: list[str] = []
+        if mouse_pos:
+            debug_texts.append(f"Mouse position: {mouse_pos.to_tuple()}")
+        if fps:
+            debug_texts.append(f"FPS: {fps:.2f}")
+        debug_texts.append(f"Game speed: {speed:.2f}")
 
-    debug_texts: list[str] = []
-    if mouse_pos:
-        debug_texts.append(f"Mouse position: {mouse_pos.to_tuple()}")
-    if fps:
-        debug_texts.append(f"FPS: {fps:.2f}")
-    debug_texts.append(f"Game speed: {speed:.2f}")
+        for i, text in enumerate(debug_texts):
+            debug_label = font.render(text, True, fg_color)
+            debug_surf.blit(debug_label, (10, 10 + i * 30))
 
-    for i, text in enumerate(debug_texts):
-        debug_label = font.render(text, True, fg_color)
-        debug_surf.blit(debug_label, (10, 10 + i * 30))
-
-    screen.blit(debug_surf, (screen_width - size[0], screen_height - size[1]))
+        screen.blit(debug_surf, (screen_width - size[0], screen_height - size[1]))
