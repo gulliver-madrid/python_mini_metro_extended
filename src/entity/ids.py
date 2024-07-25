14cd2ee60ef5fc7e2abd789bc86983295b848946
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-07-25 16:01:19 +0200

    refactor ids.py

--- src/entity/ids.py
+++ src/entity/ids.py
@@ -9,28 +9,28 @@ EntityNumId = NewType("EntityNumId", int)
 
 
 def create_new_passenger_id() -> EntityId:
-    return _create_new_entity_id_with_label("Passenger")
+    return _create_new_entity_id(label="Passenger")
 
 
 def create_new_path_id() -> EntityId:
-    return _create_new_entity_id_with_label("Path")
+    return _create_new_entity_id(label="Path")
 
 
 def create_new_path_segment_id() -> EntityId:
-    return _create_new_entity_id_with_label("PathSegment")
+    return _create_new_entity_id(label="PathSegment")
 
 
 def create_new_padding_segment_id() -> EntityId:
-    return _create_new_entity_id_with_label("PaddingSegment")
+    return _create_new_entity_id(label="PaddingSegment")
 
 
 def create_new_metro_id() -> EntityId:
-    return _create_new_entity_id_with_label("Metro")
+    return _create_new_entity_id(label="Metro")
 
 
 def create_new_station_id(shape_type: ShapeType) -> EntityId:
     return EntityId(f"Station-{uuid()}-{shape_type}")
 
 
-def _create_new_entity_id_with_label(label: str) -> EntityId:
+def _create_new_entity_id(*, label: str) -> EntityId:
     return EntityId(f"{label}-{uuid()}")
