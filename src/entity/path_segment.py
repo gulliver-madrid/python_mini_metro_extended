bd0a7eb5d29ac10b5dafb6bfeca5a70115b83d2b
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-07-26 16:33:11 +0200

    refactor PathSegment.__init__()

--- src/entity/path_segment.py
+++ src/entity/path_segment.py
@@ -1,5 +1,6 @@
 from src.config import path_order_shift, path_width
 from src.geometry.line import Line
+from src.geometry.point import Point
 from src.geometry.utils import direction
 from src.type import Color
 
@@ -21,19 +22,25 @@ class PathSegment(Segment):
         super().__init__(color, create_new_path_segment_id())
         self.stations = StationPair(start_station, end_station)
         self._path_order = path_order
-
-        start_point = start_station.position
-        end_point = end_station.position
-        direct = direction(start_point, end_point)
-        buffer_vector = (direct * path_order_shift).rotate(90)
-
-        offset_vector = buffer_vector * self._path_order
-        start = start_station.position + offset_vector
-        end = end_station.position + offset_vector
-        self.points = PointPair(start, end)
+        self.points = _get_segment_edges(self.stations, self._path_order)
         self.line = Line(
             color=self.color,
             start=self.points.start,
             end=self.points.end,
             width=path_width,
         )
+
+
+def _get_segment_edges(stations: StationPair, path_order: int) -> PointPair:
+    offset_vector = _get_offset_vector(stations, path_order)
+    start = stations.start.position + offset_vector
+    end = stations.end.position + offset_vector
+    return PointPair(start, end)
+
+
+def _get_offset_vector(stations: StationPair, path_order: int) -> Point:
+    start_point = stations.start.position
+    end_point = stations.end.position
+    direct = direction(start_point, end_point)
+    buffer_vector = (direct * path_order_shift).rotate(90)
+    return buffer_vector * path_order
