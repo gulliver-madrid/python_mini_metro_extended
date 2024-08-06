c5528588adc553542cf3609595bf4bc86c46f094
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-06 18:40:46 +0200

    failed try to track mouse last position

--- src/event/convert.py
+++ src/event/convert.py
@@ -1,13 +1,24 @@
+from typing import Final
 import pygame
 
+from src.geometry.point import Point
 from src.event.event import Event
 from src.event.keyboard import KeyboardEvent
 from src.event.mouse import MouseEvent
 from src.event.type import KeyboardEventType, MouseEventType
 from src.utils import tuple_to_point
 
+NULL_POS: Final = (0, 0)
+
+null_point: Final = tuple_to_point(NULL_POS)
+last_mouse_pos: Point = null_point
+
 
 def convert_pygame_event(event: pygame.event.Event) -> Event | None:
+    global last_mouse_pos
+    if event.type != pygame.MOUSEMOTION:
+        print(event)
+
     match event.type:
         case pygame.MOUSEBUTTONDOWN:
             mouse_position = tuple_to_point(event.pos)
@@ -17,6 +28,7 @@ def convert_pygame_event(event: pygame.event.Event) -> Event | None:
             return MouseEvent(MouseEventType.MOUSE_UP, mouse_position)
         case pygame.MOUSEMOTION:
             mouse_position = tuple_to_point(event.pos)
+            last_mouse_pos = mouse_position
             return MouseEvent(MouseEventType.MOUSE_MOTION, mouse_position)
         case pygame.KEYUP:
             return KeyboardEvent(KeyboardEventType.KEY_UP, event.key)
