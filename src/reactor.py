a0ec714aad51a2de71c86d8cf65407bfc739f15b
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-07 13:18:14 +0200

    make UI_Reactor.engine private

--- src/reactor.py
+++ src/reactor.py
@@ -15,7 +15,7 @@ from src.ui.path_button import PathButton
 
 class UI_Reactor:
     __slots__ = (
-        "engine",
+        "_engine",
         "is_mouse_down",
         "_console",
         "_last_clicked",
@@ -23,7 +23,7 @@ class UI_Reactor:
     )
 
     def __init__(self, engine: Engine) -> None:
-        self.engine = engine
+        self._engine = engine
         self._console = Console()
         self.is_mouse_down: bool = False
         self._last_clicked: Station | None = None
@@ -39,13 +39,13 @@ class UI_Reactor:
     def _try_process_console_commands(self) -> None:
         cmd = self._console.try_get_command()
         if cmd == "resume":
-            if self.engine.is_paused:
-                self.engine.toggle_pause()
+            if self._engine.is_paused:
+                self._engine.toggle_pause()
         else:
             assert cmd is None, cmd
 
     def _on_mouse_event(self, event: MouseEvent) -> None:
-        entity = self.engine.get_containing_entity(event.position)
+        entity = self._engine.get_containing_entity(event.position)
 
         if event.event_type == MouseEventType.MOUSE_DOWN:
             self.is_mouse_down = True
@@ -64,24 +64,24 @@ class UI_Reactor:
 
         match event.key:
             case pygame.K_SPACE:
-                self.engine.toggle_pause()
+                self._engine.toggle_pause()
             case pygame.K_t:
-                if self.engine.is_paused:
-                    self.engine.toggle_pause()
-                self.engine.steps_allowed = 1
+                if self._engine.is_paused:
+                    self._engine.toggle_pause()
+                self._engine.steps_allowed = 1
             case pygame.K_ESCAPE:
-                self.engine.exit()
+                self._engine.exit()
             case pygame.K_c:
-                if not self.engine.is_paused:
-                    self.engine.toggle_pause()
-                self._console.launch_console(self.engine)
+                if not self._engine.is_paused:
+                    self._engine.toggle_pause()
+                self._console.launch_console(self._engine)
             case pygame.K_d:
-                self.engine.showing_debug = not self.engine.showing_debug
+                self._engine.showing_debug = not self._engine.showing_debug
             case pygame.K_s:
-                if self.engine.game_speed == 1:
-                    self.engine.game_speed = 5
+                if self._engine.game_speed == 1:
+                    self._engine.game_speed = 5
                 else:
-                    self.engine.game_speed = 1
+                    self._engine.game_speed = 1
             case _:
                 pass
 
@@ -90,19 +90,19 @@ class UI_Reactor:
     ) -> None:
         if self._last_clicked:
             self._last_clicked = None
-        self.engine.ui.last_pos = position
+        self._engine.ui.last_pos = position
         if self.is_mouse_down:
             self._on_mouse_motion_with_mouse_down(entity, position)
         elif isinstance(entity, Button):
             entity.on_hover()
         else:
-            self.engine.ui.exit_buttons()
+            self._engine.ui.exit_buttons()
 
     def _on_mouse_down(
         self, entity: Station | PathButton | None, position: Point
     ) -> None:
-        if self.engine.path_manager.path_being_created:
-            self.engine.path_manager.abort_path_creation_or_expanding()
+        if self._engine.path_manager.path_being_created:
+            self._engine.path_manager.abort_path_creation_or_expanding()
         if isinstance(entity, Station):
             if self._last_clicked == entity:
                 self._index_clicked += 1
@@ -110,27 +110,27 @@ class UI_Reactor:
                 self._index_clicked = 0
                 self._last_clicked = entity
 
-            paths = self.engine.path_manager.get_paths_with_station(entity)
+            paths = self._engine.path_manager.get_paths_with_station(entity)
 
             num_possible_targets = len(paths) + 1
             index_clicked = self._index_clicked % num_possible_targets
 
             if index_clicked == 0:
-                self.engine.path_manager.start_path_on_station(entity)
+                self._engine.path_manager.start_path_on_station(entity)
             else:
-                self.engine.path_manager.start_expanding_path_on_station(
+                self._engine.path_manager.start_expanding_path_on_station(
                     entity, index_clicked - 1
                 )
 
         if (
             not entity
-            and self.engine.is_paused
+            and self._engine.is_paused
             and not Config.allow_self_crossing_lines
         ):
-            self.engine.try_starting_path_edition(position)
+            self._engine.try_starting_path_edition(position)
 
     def _on_mouse_up(self, entity: Station | PathButton | None) -> None:
-        path_manager = self.engine.path_manager
+        path_manager = self._engine.path_manager
         if path_manager.path_being_created:
             if isinstance(entity, Station):
                 path_manager.try_to_end_path_on_station(entity)
@@ -145,12 +145,12 @@ class UI_Reactor:
     def _on_mouse_motion_with_mouse_down(
         self, entity: Station | PathButton | None, position: Point
     ) -> None:
-        if self.engine.path_manager.path_being_created:
+        if self._engine.path_manager.path_being_created:
             if isinstance(entity, Station):
-                self.engine.path_manager.add_station_to_path(entity)
+                self._engine.path_manager.add_station_to_path(entity)
             else:
-                self.engine.path_manager.set_temporary_point(position)
-        elif self.engine.path_manager.path_being_edited:
-            self.engine.path_manager.path_being_edited.set_temporary_point(position)
+                self._engine.path_manager.set_temporary_point(position)
+        elif self._engine.path_manager.path_being_edited:
+            self._engine.path_manager.path_being_edited.set_temporary_point(position)
             if isinstance(entity, Station):
-                self.engine.path_manager.touch(entity)
+                self._engine.path_manager.touch(entity)
